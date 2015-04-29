7549252e5a7687df109261882746d6857a709d66
DevMech <agmecham@gmail.com>
2015-04-29 16:22:02 -0600

    Finished Conditional

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -19,6 +19,34 @@
 
     // Insert code here...
     
+    int karl= 10;
+    int leonard= 7;
+    
+    int karlPlace;
+    int leonardPlace;
+    
+    karlPlace = karl >7 ? 1:2;
+    leonardPlace = leonard >7 ? 1:2;
+    
+    if (karlPlace == 1 && leonardPlace == 1) {
+        
+        NSLog(@"Both equal to 1");
+    }
+    
+    else if (karlPlace == 1) {
+        
+        NSLog(@"karl is placed 1 and leonard is placed 2");
+    }
+    
+    else if (leonardPlace == 1) {
+        
+        NSLog(@"Leonard is placed 1 and Karl is placed 2");
+    }
+    
+    else  {
+        
+        NSLog(@"Karl and Leonard are placed 2");
+    }
     
     return YES;
 }
